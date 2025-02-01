60770608d68ec1ec4e8eabc4a7d6ec465839edf4
Chris Allison <chrisallisonsax@gmail.com>
2025-02-01 18:03:57 -0500

    server test

---
+++ client.py
@@ -0,0 +1,37 @@
+import socket
+import threading
+
+SERVER_IP = input("Enter the server IP: ")  # Get the host's IP address
+PORT = 5000  # Same port as the server
+
+nickname = input("Choose a nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, PORT))
+
+def receive_messages():
+    """Receive messages from the server and print them."""
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == "NICKNAME":
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("Disconnected from the server.")
+            client.close()
+            break
+
+def send_messages():
+    """Send messages to the server."""
+    while True:
+        message = input("")
+        client.send(f"{nickname}: {message}".encode('utf-8'))
+
+# Start threads for receiving and sending messages
+receive_thread = threading.Thread(target=receive_messages)
+receive_thread.start()
+
+send_thread = threading.Thread(target=send_messages)
+send_thread.start()
---
+++ server.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+
+# Server setup
+HOST = '0.0.0.0'  # Listen on all available network interfaces
+PORT = 5001        # Port for the chat room
+
+clients = []
+nicknames = []
+
+def broadcast(message, sender_socket=None):
+    """Send message to all clients except the sender."""
+    for client in clients:
+        if client != sender_socket:
+            client.send(message)
+
+def handle_client(client):
+    """Handle communication from a single client."""
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message, client)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames.pop(index)
+            broadcast(f"{nickname} has left the chat.".encode('utf-8'))
+            break
+
+def receive_connections():
+    """Accept new connections and start a new thread for each client."""
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # Allow address reuse
+    server.bind((HOST, PORT))
+    server.listen()
+
+    print(f"Server started on {HOST}:{PORT}")
+
+    while True:
+        client, address = server.accept()
+        print(f"New connection from {address}")
+
+        client.send("NICKNAME".encode('utf-8'))
+        nickname = client.recv(1024).decode('utf-8')
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of new client: {nickname}")
+        broadcast(f"{nickname} joined the chat!".encode('utf-8'))
+        client.send("Connected to the chat room!".encode('utf-8'))
+
+        thread = threading.Thread(target=handle_client, args=(client,))
+        thread.start()
+
+receive_connections()
